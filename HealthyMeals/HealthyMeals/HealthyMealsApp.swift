08a586f618ea72bef9a5d8f6b24062d1b8c738d5
Martin Wiser <martinwiser@Martins-MacBook-Air-2.local>
2025-05-09 10:43:54 -0600

    initial app

---
+++ HealthyMeals/HealthyMeals/HealthyMealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthyMealsApp.swift
+//  HealthyMeals
+//
+//  Created by Martin Wiser on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthyMealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthyMeals/HealthyMealsTests/HealthyMealsTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthyMealsTests.swift
+//  HealthyMealsTests
+//
+//  Created by Martin Wiser on 5/9/25.
+//
+
+import Testing
+@testable import HealthyMeals
+
+struct HealthyMealsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
